d4f9ba2aaccd88dd0d1575e31f1658c3a2b5876a
Zivi Weinstock <zivi.weinstock@gmail.com>
2018-06-13 19:25:52 -0700

    Link url fix

--- components/footer.js
+++ components/footer.js
@@ -1,12 +1,11 @@
 class Footer extends React.Component {
-
   render() {
     return (
       <div>
         Made with 🐩 by &nbsp;
-        <a href="https://github.com/Zivi/plotPlotter">Zivi Weinstock</a>
+        <a href="https://github.com/Zivi/barkpark">Zivi Weinstock</a>
       </div>
-    )
+    );
   }
 }
 export default Footer;
